905e37deac638a671cb2eda098f7712607b51196
Seamile <lanhuermao@gmail.com>
2018-02-27 11:22:55 +0800

    修改 redis 作为缓存引擎

--- geekpy/settings.py
+++ geekpy/settings.py
@@ -119,3 +119,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "PICKLE_VERSION": -1,
+        }
+    }
+}
