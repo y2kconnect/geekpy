33bb139e13b6a200520c294fcf317c8e4a2c2737
Seamile <lanhuermao@gmail.com>
2018-03-01 09:38:35 +0800

    修改为根据 session 区分不同用户

--- post/middleware.py
+++ post/middleware.py
@@ -1,7 +1,5 @@
 import time
 
-from django.core.cache import cache
-
 
 def simple_middleware(view_func):
     def wrap(request):
@@ -14,23 +12,22 @@ def simple_middleware(view_func):
 
 def request_rate(get_response):
     def wrap(request):
-        # 1. session 只有登陆的情况下才能使用吗？
-        # 2. 通过 IP 记录的话，怎么解决同局域网下的访问
+        # 1. session 不是只有登陆的情况下才能使用吗。
+        # 2. 同局域网下的访问，只能通过 session 来区分不同用户
 
         # 访问时间
         # 第 1 次  100000.000
         # 第 2 次  100000.500
         # 第 3 次  100009.000
 
-        key = 'RequestTime-%s' % request.META['REMOTE_ADDR']
-        history = cache.get(key, [0, 0])  # 取出历史访问记录
+        history = request.session.get('req_history', [0, 0])  # 从 Session 里取出历史访问记录
         now = time.time()
         if (now - history[0]) < 1:  # 判断距离上上次访问的时间间隔是否小于 1 秒
-            time.sleep(2)
+            time.sleep(30)
 
-        # 跟新本次访问时间
+        # 更新本次访问时间
         history.append(time.time())
-        cache.set(key, history[1:])
+        request.session['req_history'] = history[1:]
 
         return get_response(request)
     return wrap
